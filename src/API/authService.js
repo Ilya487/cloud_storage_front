b6cf312b9e5698c0a75ed4cd81af3e94f0ad1808
Ilya <poctadladota@gmail.com>
2025-01-24 02:04:59 +0900

    написал апи сервисы для управления пользователем

---
+++ src/API/authService.js
@@ -0,0 +1,67 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { SERVER_URL } from "./config";
+
+async function checkAuth() {
+  const response = await fetch(SERVER_URL + "/check-auth", {
+    credentials: "include",
+  });
+
+  return response.json();
+}
+
+async function signinUser({ login, password }) {
+  const response = await fetch(SERVER_URL + "/signin", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({ login, password }),
+  });
+
+  if (response.ok) return response.json();
+  else {
+    const errorData = await response.json();
+    throw new Error(errorData.message);
+  }
+}
+
+async function logOut() {
+  const response = await fetch(SERVER_URL + "/logout", {
+    credentials: "include",
+    method: "POST",
+  });
+
+  return response.json();
+}
+
+async function signupUser({ login, password }) {
+  const response = await fetch(SERVER_URL + "/signup", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({ login, password }),
+  });
+
+  if (response.ok) return response.json();
+  else {
+    const errorData = await response.json();
+    throw new Error(errorData.message);
+  }
+}
+
+export const useAuth = () =>
+  useQuery({
+    queryKey: ["auth"],
+    queryFn: checkAuth,
+    staleTime: 1000 * 60 * 25,
+  });
+
+export const useSignin = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: signinUser,
+    onSuccess: () => queryClient.invalidateQueries(["auth"]),
+  });
+};
+
+export const useLogOut = () => useMutation({ mutationFn: logOut });
+
+export const useSignUp = () => useMutation({ mutationFn: signupUser });
