a25ef2caf9b6ed490727fad4b907e4d139170934
Ilya <poctadladota@gmail.com>
2025-02-02 03:18:40 +0900

    написал компонент FolderContextMenu

---
+++ src/Components/FolderContextMenu/FolderContextMenu.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import ContextMenu from "../ContextMenu/ContextMenu";
+import RenameDialog from "../RenameDialog/RenameDialog";
+
+const FolderContextMenu = ({
+  dirId,
+  defaultName,
+  onRename,
+  onClose,
+  contextMenuEvent,
+  contextMenuVisible,
+}) => {
+  const updatedOnClose = () => {
+    disableActiveOptions();
+    onClose && onClose();
+  };
+
+  const [optionsVisible, setOptionsVisible] = useState({
+    rename: false,
+  });
+
+  function handleOptionClick(option, value) {
+    setOptionsVisible({ ...optionsVisible, [option]: value });
+  }
+
+  function disableActiveOptions() {
+    const updatedState = { ...optionsVisible };
+    for (const option in updatedState) {
+      updatedState[option] = false;
+    }
+
+    setOptionsVisible(updatedState);
+  }
+
+  useEffect(disableActiveOptions, []);
+
+  const optionsSettings = [
+    {
+      label: "Переименовать",
+      action: () => handleOptionClick("rename", true),
+    },
+    { label: "Скачать" },
+    { label: "Переместить" },
+    { label: "Удалить" },
+  ];
+
+  return (
+    <>
+      {contextMenuVisible && (
+        <ContextMenu
+          menuOptions={optionsSettings}
+          contextMenuEvent={contextMenuEvent}
+        />
+      )}
+      {optionsVisible.rename && (
+        <RenameDialog
+          dirId={dirId}
+          defaultName={defaultName}
+          onRename={onRename}
+          onClose={updatedOnClose}
+        />
+      )}
+    </>
+  );
+};
+
+export default FolderContextMenu;
