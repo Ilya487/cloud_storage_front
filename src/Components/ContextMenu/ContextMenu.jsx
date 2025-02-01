b9958923553e807905845ab0921fa44c5ffcdb19
Ilya <poctadladota@gmail.com>
2025-02-02 02:12:11 +0900

    написал компонет для отображения кастомного контекстого меню

---
+++ src/Components/ContextMenu/ContextMenu.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./ContextMenu.module.css";
+
+const ContextMenu = ({ menuOptions, contextMenuEvent }) => {
+  const [menuCoords, setMenuCoords] = useState({ x: 0, y: 0 });
+  const [calcCoordComplete, setCalcCoordComplete] = useState(false);
+  const menu = useRef();
+
+  function calculateCoord() {
+    const result = {};
+    contextMenuEvent.preventDefault();
+
+    result.x = contextMenuEvent.clientX;
+    result.y = contextMenuEvent.clientY;
+
+    const menuWidth = menu.current.offsetWidth;
+    const menuHeight = menu.current.offsetHeight;
+
+    if (result.x + menuWidth > window.innerWidth) {
+      result.x = result.x - menuWidth;
+    }
+
+    if (result.y + menuHeight > window.innerHeight) {
+      result.y = result.y - menuHeight;
+    }
+
+    setMenuCoords(result);
+    setCalcCoordComplete(true);
+  }
+
+  useEffect(calculateCoord, [contextMenuEvent]);
+
+  return (
+    <ul
+      className={styles.menu}
+      ref={menu}
+      style={{
+        top: menuCoords.y + "px",
+        left: menuCoords.x + "px",
+        visibility: calcCoordComplete ? "visible" : "hidden",
+      }}
+    >
+      {menuOptions.map((item, index) => (
+        <li key={index} onClick={item.action} className={styles["menu-item"]}>
+          {item.label}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContextMenu;
