bc6157477ffb4cbc394fe810d5d7be5b8325f947
Ilya <poctadladota@gmail.com>
2025-02-11 20:18:09 +0900

    добавил возможность прокинуть свои имена классов стилей в ModalWindow

--- src/Components/ModalWindow/ModalWindow.jsx
+++ src/Components/ModalWindow/ModalWindow.jsx
@@ -1,11 +1,17 @@
 import { forwardRef } from "react";
 import styles from "./ModalWindow.module.css";
 
-const ModalWindow = forwardRef(function ModalWindow({ children }, ref) {
+const ModalWindow = forwardRef(function ModalWindow(
+  { children, ...args },
+  ref
+) {
   return (
     <>
       <div className={styles.overlay}></div>
-      <div className={styles["modal-container"]} ref={ref}>
+      <div
+        className={`${styles["modal-container"]} ${args.className || ""}`}
+        ref={ref}
+      >
         {children}
       </div>
     </>
