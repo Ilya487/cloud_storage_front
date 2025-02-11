6fc730529cc7a9ebf07a51879d58ab65d0bf69d8
Ilya <poctadladota@gmail.com>
2025-02-11 20:17:24 +0900

    Написал дополнительные компоненты для MoveDialog

---
+++ src/Components/MoveDialog/CatalogList.jsx
@@ -0,0 +1,49 @@
+import { TiCloudStorage } from "react-icons/ti";
+import styles from "./MoveDialog.module.css";
+import { FaFolder } from "react-icons/fa";
+import clsx from "clsx";
+
+const CatalogList = ({
+  catalog,
+  onSelect,
+  onMoveInDir,
+  selectedDirId,
+  showMoveToRoot,
+}) => {
+  return (
+    <ul className={styles["dir-list"]}>
+      {catalog.map((item) => (
+        <li
+          key={item.id}
+          onClick={() => onSelect(item.id)}
+          onDoubleClick={() => onMoveInDir(item.id)}
+          className={clsx(
+            styles["catalog-item"],
+            item.id == selectedDirId && styles["catalog-item--selected"]
+          )}
+        >
+          <FaFolder size={35} color="#3030e7" className={styles["dir-icon"]} />
+          <span>{item.name}</span>
+        </li>
+      ))}
+      {showMoveToRoot && (
+        <li
+          onClick={() => onSelect("root")}
+          className={clsx(
+            styles["catalog-item"],
+            selectedDirId == "root" && styles["catalog-item--selected"]
+          )}
+        >
+          <TiCloudStorage
+            size={35}
+            color="#3030e7"
+            className={styles["dir-icon"]}
+          />
+          <span>Мой диск</span>
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default CatalogList;
