548d3f6819e73b74e3ae1e23e9d04d8444974788
Ilya <poctadladota@gmail.com>
2025-02-25 03:41:21 +0900

    Добавил поддержку управления подготовкой и отменой загрузок файлов в UploadContext

--- src/context/UploadContext.jsx
+++ src/context/UploadContext.jsx
@@ -1,60 +1,120 @@
-import { createContext, useContext, useRef, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { FileSender } from "../API/FileSender";
 
 const UploadContext = createContext();
 
 export const UploadProvider = ({ children }) => {
   const [activeUploads, setActiveUploads] = useState([]);
+  const [preparingUploads, setPreparingUploads] = useState([]);
+  const [cancelUploads, setCancelUploads] = useState([]);
 
-  function addUploads(files) {
+  function addUploads(files, destinationDirId) {
     if (activeUploads.size == 3) return;
     if (files instanceof FileList) {
-      [...files].forEach((file) => createUploadSession(file));
+      [...files].forEach((file) => createUploadSession(file, destinationDirId));
     }
   }
 
-  async function createUploadSession(file) {
-    const fileSender = new FileSender(file);
+  async function createUploadSession(file, destinationDirId) {
+    const fileSender = new FileSender(file, destinationDirId);
+    const generatedKey = file.name + Date.now();
 
     fileSender.onChunkLoad = ({ progress }) => {
-      upadteSessionProgress(sessionId, progress);
+      updateSessionProgress(sessionId, progress);
     };
 
-    const sessionId = await fileSender.startSending();
+    fileSender.onStatusUpdate = (status) => {
+      if (status == FileSender.STATUS_PREPARING) {
+        updatePreparingSessionStatus(generatedKey, status);
+      } else {
+        deletePreparingSession(generatedKey);
+        updateSessionStatus(sessionId, status);
+      }
+    };
+
+    fileSender.onError = (message) => {
+      if (fileSender.getStatus() == FileSender.STATUS_PREPARING) {
+        cancelPreparingSession(generatedKey, message);
+      } else cancelActiveSession(sessionId, message);
+    };
+
+    addPreparingSession({
+      id: generatedKey,
+      file,
+      status: fileSender.getStatus(),
+      destinationDirId,
+    });
+    const sessionId = await fileSender.initialize();
+    fileSender.start();
 
     addUploadSession({
       id: sessionId,
-      fileName: file.name,
+      file,
       status: fileSender.getStatus(),
-      cancelUpload: () => {
-        fileSender.cancelSending();
-        cancelSession(sessionId);
+      destinationDirId,
+      cancelUpload: async () => {
+        if (fileSender.getStatus() !== FileSender.STATUS_SENDING) return;
+        await fileSender.cancelSending();
+        cancelActiveSession(sessionId, "Отменен");
       },
     });
   }
 
-  function cancelSession(id) {
-    setActiveUploads((uploads) =>
-      uploads.map((session) =>
-        session.id == id ? { ...session, progress: 0 } : session
-      )
-    );
+  function cancelActiveSession(id, reason) {
+    setActiveUploads((currentActiveUploads) => {
+      const canceledSession = currentActiveUploads.find(
+        (session) => session.id == id
+      );
+      addCancelSession(canceledSession, reason);
+
+      return currentActiveUploads.filter((session) => session.id !== id);
+    });
   }
 
-  function addUploadSession({ id, fileName, cancelUpload, status }) {
+  function cancelPreparingSession(id, reason) {
+    setPreparingUploads((currentPreapringUploads) => {
+      const canceledSession = currentPreapringUploads.find(
+        (session) => session.id == id
+      );
+      addCancelSession(canceledSession, reason);
+
+      return currentPreapringUploads.filter((session) => session.id !== id);
+    });
+  }
+
+  function addUploadSession({
+    id,
+    file,
+    cancelUpload,
+    status,
+    destinationDirId,
+  }) {
     setActiveUploads((uploads) => [
       ...uploads,
       {
         id,
-        fileName,
+        file,
         progress: 0,
         cancelUpload,
         status,
+        destinationDirId,
       },
     ]);
   }
 
-  function upadteSessionProgress(id, progress) {
+  function addPreparingSession({ id, file, status, destinationDirId }) {
+    setPreparingUploads((uploads) => [
+      ...uploads,
+      {
+        id,
+        file,
+        status,
+        destinationDirId,
+      },
+    ]);
+  }
+
+  function updateSessionProgress(id, progress) {
     setActiveUploads((uploads) =>
       uploads.map((session) =>
         session.id == id ? { ...session, progress } : session
@@ -62,10 +122,77 @@ export const UploadProvider = ({ children }) => {
     );
   }
 
-  function updateSessionStatus(id, newStatus) {}
+  function updateSessionStatus(id, newStatus) {
+    setActiveUploads((uploads) =>
+      uploads.map((session) =>
+        session.id == id ? { ...session, status: newStatus } : session
+      )
+    );
+  }
+
+  function updatePreparingSessionStatus(id, newStatus) {
+    setPreparingUploads((uploads) =>
+      uploads.map((session) =>
+        session.id == id ? { ...session, status: newStatus } : session
+      )
+    );
+  }
+
+  function deletePreparingSession(id) {
+    setPreparingUploads((uploads) =>
+      uploads.filter((session) => session.id != id)
+    );
+  }
+
+  function deleteCancelSession(id) {
+    setCancelUploads((uploads) =>
+      uploads.filter((session) => session.id != id)
+    );
+  }
+
+  function addCancelSession(canceledSession, reason) {
+    setCancelUploads((uploads) => {
+      if (uploads.some((session) => session.id == canceledSession.id))
+        return uploads;
+      return [
+        ...uploads,
+        {
+          ...canceledSession,
+          reason,
+          restart: () => {
+            deleteCancelSession(canceledSession.id);
+            createUploadSession(
+              canceledSession.file,
+              canceledSession.destinationDirId
+            );
+          },
+          delete: () => {
+            deleteCancelSession(canceledSession.id);
+          },
+        },
+      ];
+    });
+  }
+
+  function calcCountActiveUpload() {
+    const activeCount = activeUploads.reduce((acc, item) => {
+      if (item.status !== FileSender.STATUS_COMPLETE) return ++acc;
+      else return acc;
+    }, 0);
+
+    return activeCount + preparingUploads.length;
+  }
 
   return (
-    <UploadContext.Provider value={{ addUploads, activeUploads }}>
+    <UploadContext.Provider
+      value={{
+        addUploads,
+        activeUploads,
+        preparingUploads,
+        cancelUploads,
+        countOfActiveUpload: calcCountActiveUpload(),
+      }}
+    >
       {children}
     </UploadContext.Provider>
   );
