438135624f8eed6f5acc8493ac9d31bcd47f3a79
Ilya <poctadladota@gmail.com>
2025-06-19 22:02:47 +0900

    Добавлен компонент CatalogFilter для фильтрации каталога

---
+++ src/Components/CatalogFilter/CatalogFilter.jsx
@@ -0,0 +1,63 @@
+import { FaArrowDown } from "react-icons/fa";
+import styles from "./CatalogFilter.module.css";
+import clsx from "clsx";
+import { useRef } from "react";
+
+const CatalogFilter = ({ filterSetup, setFilterSetup }) => {
+  const activeFilter = useRef();
+
+  function findActiveFilter() {
+    for (let filter in filterSetup) {
+      if (filterSetup[filter]) {
+        return filter;
+      }
+    }
+  }
+
+  if (activeFilter.current === undefined) {
+    activeFilter.current = findActiveFilter();
+  }
+
+  function setFilter(filterName) {
+    const updatedFilter = { ...filterSetup };
+
+    if (filterName == activeFilter.current) {
+      updatedFilter.ascending = !updatedFilter.ascending;
+    } else {
+      updatedFilter[activeFilter.current] = false;
+      updatedFilter[filterName] = true;
+      activeFilter.current = filterName;
+      updatedFilter.ascending = true;
+    }
+
+    setFilterSetup(updatedFilter);
+  }
+
+  return (
+    <li className={styles["catalog-headers"]}>
+      <div className={styles["filter-item"]}>
+        <span onClick={() => setFilter("name")}>Название</span>
+        <FaArrowDown
+          display={filterSetup.name ? "true" : "none"}
+          className={clsx(styles.arrow, filterSetup.ascending && styles["arrow--up"])}
+        />
+      </div>
+      <div className={styles["filter-item"]}>
+        <span onClick={() => setFilter("date")}>Дата создания</span>{" "}
+        <FaArrowDown
+          display={filterSetup.date ? "true" : "none"}
+          className={clsx(styles.arrow, filterSetup.ascending && styles["arrow--up"])}
+        />
+      </div>
+      <div className={styles["filter-item"]}>
+        <span onClick={() => setFilter("size")}>Размер файла</span>
+        <FaArrowDown
+          display={filterSetup.size ? "true" : "none"}
+          className={clsx(styles.arrow, filterSetup.ascending && styles["arrow--up"])}
+        />
+      </div>
+    </li>
+  );
+};
+
+export default CatalogFilter;
