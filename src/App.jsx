10b64fcb84b8a025541dcaa38624f5f3fec85bdb
ilya <poctadladota@gmail.com>
2025-07-21 21:36:41 +0900

    подправил роуты

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Navigate, Route, Routes } from "react-router";
 import Login from "./pages/Login/Login";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import AuthQuard from "./utils/AuthQuard";
 import Catalog from "./pages/Catalog/Catalog";
 import Layout from "./Components/Layout/Layout";
 import { UploadProvider } from "./context/UploadContext";
 import Upload from "./pages/Upload/Upload";
 import { ToastContainer } from "react-toastify";
+import PrivateRoute from "./routing/PrivateRoute";
+import PublicRoute from "./routing/PublicRoute";
 
 function App() {
   const queryClient = new QueryClient();
@@ -17,12 +18,18 @@ function App() {
       <UploadProvider>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route path="login" element={<Login />} />
-            <Route element={<AuthQuard />}>
-              <Route path="catalog" element={<Navigate to={"/catalog/root"} />} />
-              <Route path="catalog/:dirId" element={<Catalog />} />
+            <Route element={<PublicRoute />}>
+              <Route path="login" element={<Login />} />
+            </Route>
+            <Route element={<PrivateRoute />}>
+              <Route index element={<Navigate to="/catalog" replace />} />
+              <Route path="catalog">
+                <Route index element={<Navigate to="root" />} />
+                <Route path=":dirId" element={<Catalog />} />
+              </Route>
               <Route path="/upload" element={<Upload />} />
             </Route>
+            <Route path="*" element={<h1>Page not found</h1>} />
           </Route>
         </Routes>
         <ToastContainer />
