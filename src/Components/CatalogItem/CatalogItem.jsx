c8a13ebd336774d6ee64b70712b755bdd2794b8c
ilya <poctadladota@gmail.com>
2025-08-22 22:17:56 +0900

    вынес из catalogItem обработку контекстного меню

--- src/Components/CatalogItem/CatalogItem.jsx
+++ src/Components/CatalogItem/CatalogItem.jsx
@@ -2,18 +2,13 @@ import { useNavigate, useParams } from "react-router";
 import styles from "./CatalogItem.module.css";
 import { FaFile, FaFolder } from "react-icons/fa";
 import useOutsideHandle from "../../hooks/useOutsideHandle";
-import { useRefreshFolderContent } from "../../API/fileSystemService";
-import useContextMenu from "../../hooks/useContextMenu";
-import ItemContextMenu from "../ItemContextMenu/ItemContextMenu";
 import clsx from "clsx";
 import fileSizeDisplay from "../../utils/fileSizeDisplay";
 
-const CatalogItem = ({ catalogItem }) => {
+const CatalogItem = ({ catalogItem, handleContextMenu, closeMenu, isSelected }) => {
   const { id, name, type, created_at, size } = catalogItem;
   const { dirId } = useParams();
-  const { isOpen: cntxMenuVisible, position, closeMenu, handleContextMenu } = useContextMenu();
   const navigator = useNavigate();
-  const refreshFolder = useRefreshFolderContent(dirId);
 
   function openFolder() {
     if (type != "folder") return;
@@ -24,40 +19,27 @@ const CatalogItem = ({ catalogItem }) => {
     navigator(updatedPath);
   }
 
-  const folderRef = useOutsideHandle(["click", "contextmenu"], () => closeMenu());
+  const folderRef = useOutsideHandle(["click"], () => closeMenu());
 
   return (
-    <>
-      <li
-        onDoubleClick={openFolder}
-        className={clsx(
-          styles["catalog-item"],
-          cntxMenuVisible && styles["catalog-item--selected"]
+    <li
+      onDoubleClick={openFolder}
+      className={clsx(styles["catalog-item"], isSelected && styles["catalog-item--selected"])}
+      onContextMenu={handleContextMenu}
+      ref={folderRef}
+      onClick={() => closeMenu()}
+    >
+      <div className={styles.filename} title={name}>
+        {type == "folder" ? (
+          <FaFolder color="#3030e7" className={styles.icon} />
+        ) : (
+          <FaFile color="#3030e7" className={styles.icon} />
         )}
-        onContextMenu={handleContextMenu}
-        ref={folderRef}
-        onClick={() => closeMenu()}
-      >
-        <div className={styles.filename} title={name}>
-          {type == "folder" ? (
-            <FaFolder color="#3030e7" className={styles.icon} />
-          ) : (
-            <FaFile color="#3030e7" className={styles.icon} />
-          )}
-          <span>{name}</span>
-        </div>
-        <div>{created_at}</div>
-        <div>{size ? fileSizeDisplay(size) : "—"}</div>
-      </li>
-
-      <ItemContextMenu
-        coords={position}
-        item={catalogItem}
-        onRename={refreshFolder}
-        onDelete={refreshFolder}
-        contextMenuVisible={cntxMenuVisible}
-      />
-    </>
+        <span>{name}</span>
+      </div>
+      <div>{created_at}</div>
+      <div>{size ? fileSizeDisplay(size) : "—"}</div>
+    </li>
   );
 };
 
