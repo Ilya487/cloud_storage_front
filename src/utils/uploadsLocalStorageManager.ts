8aa19922b3590f85b7e373bf340eb4f7d791126e
ilya <poctadladota@gmail.com>
2026-04-20 20:34:45 +0900

    feat: add uploadsLocalStorageManager utility for managing upload sessions

---
+++ src/utils/uploadsLocalStorageManager.ts
@@ -0,0 +1,50 @@
+const storageKey = "uploads";
+
+export interface StoredUploadSession {
+  readonly sessionId: number,
+  readonly fileInfo: {
+    name: string,
+    size: number,
+    lastModified: number
+  }
+}
+
+export const uploadsLocalStorageManager = {
+  getUploads(): StoredUploadSession[] {
+    const json = localStorage.getItem(storageKey);
+    if (json === null) return []
+    const items = JSON.parse(json) ?? [];
+    return items;
+  },
+
+  clear() {
+    localStorage.removeItem(storageKey);
+  },
+
+  deleteItem(...sessionIds: number[]) {
+    const items = this.getUploads();
+    const filterItems = items.filter(upload => !sessionIds.includes(upload.sessionId));
+
+    this.saveItems(filterItems);
+  },
+
+  addItem(file: File, sessionId: number) {
+    const newUpload: StoredUploadSession = {
+      sessionId,
+      fileInfo: {
+        name: file.name,
+        size: file.size,
+        lastModified: file.lastModified,
+      }
+    };
+
+    const uploads = this.getUploads();
+    uploads.push(newUpload);
+
+    this.saveItems(uploads);
+  },
+
+  saveItems(items: StoredUploadSession[]) {
+    localStorage.setItem(storageKey, JSON.stringify(items));
+  },
+};
