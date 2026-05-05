fdc1c6d4c1dd9b2319b5d8d1a74c3231cfc2b76b
ilya <poctadladota@gmail.com>
2026-05-05 22:54:19 +0900

    написал ChunkSelector для выбора чанков из прерванной сессии

--- src/API/FileSender.ts
+++ src/API/FileSender.ts
@@ -250,7 +250,11 @@ class ChunkSender {
   }
 
   start() {
-    this.sendGroupRequests();
+    if (!this.chunkSelector.hasNext()) {
+      this.onComplete && this.onComplete();
+    }
+    else
+      this.sendGroupRequests();
   }
 
   cancelSending() {
@@ -324,6 +328,7 @@ class ChunkSender {
 
 interface ChunkSelectStrategy {
   next: () => number | false;
+  hasNext: () => boolean;
 }
 
 class SequentialChunkSelector implements ChunkSelectStrategy {
@@ -338,5 +343,41 @@ class SequentialChunkSelector implements ChunkSelectStrategy {
     const next = this.current++;
     if (next > this.total) return false;
     return next;
+  }
+
+  hasNext() {
+    if (this.current >= this.total) return false;
+    else return true;
+  }
+}
+
+class ResumedChunkSelector implements ChunkSelectStrategy {
+  private notReadyChunks: number[];
+  private currentIndex: number = 0;
+
+  constructor(readyChunks: number[], chunksCount: number) {
+    this.notReadyChunks = this.getNotReadyChunks(readyChunks, chunksCount);
+  }
+
+  next() {
+    const nextIndex = this.currentIndex++;
+    if (nextIndex >= this.notReadyChunks.length) return false;
+    else return this.notReadyChunks[nextIndex];
+  }
+
+  hasNext() {
+    if (this.currentIndex >= this.notReadyChunks.length) return false;
+    else return true;
+  }
+
+  private getNotReadyChunks(readyChunks: number[], chunksCount: number): number[] {
+    const ready = new Set(readyChunks);
+
+    const needSend = [];
+    for (let i = 1; i <= chunksCount; i++) {
+      if (!ready.has(i)) needSend.push(i);
+    }
+
+    return needSend;
   }
 }
