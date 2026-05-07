13151758678e051547852ad46e358990a0aee809
ilya <poctadladota@gmail.com>
2026-05-07 22:20:51 +0900

    сделал абстрактный класс FileSender и спец класс, который загружает файл с нуля

--- src/API/FileSender/FileSender.ts
+++ src/API/FileSender/FileSender.ts
@@ -1,7 +1,6 @@
-import type { UploadResumeData } from "../../RestoreUploads/types";
-import apiRequest, { type DefaultErrorBody } from "../apiRequest";
+import apiRequest from "../apiRequest";
 import { SERVER_URL } from "../config";
-import { ResumedChunkSelector, SequentialChunkSelector, type ChunkSelectStrategy } from "./ChunkSelectStrategy";
+import { type ChunkSelectStrategy } from "./ChunkSelectStrategy";
 import { ChunkSender, type ChunkUploadResponse } from "./ChunkSender";
 
 type SendingStatus = 'sending' | 'cancel' | 'notRunning' | 'preparing' | 'complete' | 'canceling' | 'building';
@@ -17,18 +16,7 @@ export interface SessionIniResponse {
   sessionId: number;
 }
 
-type FileSenderConstruct =
-  | {
-    file: File,
-    destinationDirId: number | 'root';
-    retriesCount: number;
-  }
-  | {
-    resumeData: UploadResumeData,
-    retriesCount: number;
-  };
-
-export class FileSender {
+export abstract class FileSender {
   static STATUS_SENDING = "sending";
   static STATUS_CANCEL = "cancel";
   static STATUS_NOT_RUNNING = "notRunning";
@@ -40,58 +28,33 @@ export class FileSender {
   public onChunkLoad?: (data: ChunkUploadResponse) => void;
   public onFileLoad?: () => void;
   public onStatusUpdate?: (status: SendingStatus) => void;
-  public onSessionIni?: (sessionId: number, filePath: string) => void;
   public onError?: (message: string) => void;
 
-  private status: SendingStatus = 'notRunning';
-  private file: File;
-  private destinationDirId: number | "root";
-  private retriesCount: number;
-  private serverUrl: string = SERVER_URL;
-  private apiClient = apiRequest();
-  private sessionInfo?: SessionIniResponse;
-  private chunkSender?: ChunkSender;
+  protected status: SendingStatus = 'notRunning';
+  protected file: File;
+  protected destinationDirId: number | "root";
+  protected retriesCount: number;
+  protected serverUrl: string = SERVER_URL;
+  protected apiClient = apiRequest();
+  protected sessionInfo!: SessionIniResponse;
+  protected chunkSender!: ChunkSender;
 
-  constructor(params: FileSenderConstruct) {
-    if ('resumeData' in params) {
-      this.sessionInfo = {
-        chunksCount: params.resumeData.chunksCount,
-        chunkSize: params.resumeData.chunkSize,
-        path: params.resumeData.path,
-        sessionId: params.resumeData.id,
-      };
-
-      this.file = params.resumeData.file;
-      this.retriesCount = params.retriesCount;
-      this.destinationDirId = params.resumeData.destinationDirId;
-
-      this.chunkSender = this.createChunkSender(this.sessionInfo, new ResumedChunkSelector(params.resumeData.readyChunks, params.resumeData.chunksCount));
-    }
-    else {
-      this.file = params.file;
-      this.retriesCount = params.retriesCount;
-      this.destinationDirId = params.destinationDirId;
-      this.retriesCount = params.retriesCount;
-    }
+  protected constructor(file: File, destinationDirId: number | 'root', retriesCount: number) {
+    this.file = file;
+    this.retriesCount = retriesCount;
+    this.destinationDirId = destinationDirId;
+    this.retriesCount = retriesCount;
   }
 
-  async start() {
-    const sessionInfo = await this.initialize();
-    this.sessionInfo = sessionInfo;
-
-    this.chunkSender = this.createChunkSender(sessionInfo, new SequentialChunkSelector(this.sessionInfo.chunksCount));
-
-    this.updateStatus('sending');
-    this.chunkSender.start();
-  }
+  abstract start(): void;
 
   async cancelSending() {
-    if (this.status !== 'sending')
+    if (this.status !== 'sending' || !this.sessionInfo)
       throw new Error('Невозможно отменить загрузку');
 
     this.updateStatus('canceling');
 
-    this.chunkSender?.cancelSending();
+    this.chunkSender.cancelSending();
 
     await this.sendCancelRequest();
     this.updateStatus('cancel');
@@ -101,15 +64,7 @@ export class FileSender {
     return this.status;
   }
 
-  private async initialize() {
-    this.updateStatus('preparing');
-    const sessionInfo = await this.sendInitializeRequest();
-    this.onSessionIni?.(sessionInfo.sessionId, sessionInfo.path);
-
-    return sessionInfo;
-  }
-
-  private createChunkSender(sessionInfo: SessionIniResponse, chunkSelector: ChunkSelectStrategy) {
+  protected createChunkSender(sessionInfo: SessionIniResponse, chunkSelector: ChunkSelectStrategy) {
     const chunkSender = new ChunkSender({
       file: this.file,
       requestsPerRun: 4,
@@ -129,35 +84,12 @@ export class FileSender {
     return chunkSender;
   }
 
-  private async sendInitializeRequest(): Promise<SessionIniResponse> {
-    const errorHandler = (errorData: DefaultErrorBody) => {
-      this.updateStatus('cancel');
-      this.handleError(errorData.message);
-    };
-
-    const data = await this.apiClient<SessionIniResponse>({
-      url: `${this.serverUrl}/upload/init`,
-      options: {
-        credentials: "include",
-        method: "POST",
-        body: JSON.stringify({
-          fileName: this.file.name,
-          fileSize: this.file.size,
-          destinationDirId: this.destinationDirId,
-        })
-      },
-      errorHandler,
-    });
-
-    return data;
-  }
-
   private async sendCancelRequest() {
     const errorHandler = () => {
       this.handleError("Ошибка отмены запроса");
     };
     await this.apiClient({
-      url: `${this.serverUrl}/upload/cancel/${this.sessionInfo!.sessionId}`,
+      url: `${this.serverUrl}/upload/cancel/${this.sessionInfo.sessionId}`,
       options: {
         credentials: "include",
         method: "DELETE",
@@ -166,7 +98,7 @@ export class FileSender {
     });
   }
 
-  private async startBuild() {
+  protected async startBuild() {
     this.updateStatus('building');
 
     const errorHandler = () => {
@@ -175,7 +107,7 @@ export class FileSender {
     };
 
     await this.apiClient({
-      url: `${this.serverUrl}/upload/${this.sessionInfo!.sessionId}/build`,
+      url: `${this.serverUrl}/upload/${this.sessionInfo.sessionId}/build`,
       options: {
         credentials: "include",
         method: "POST",
@@ -209,7 +141,7 @@ export class FileSender {
 
     const data = await this.apiClient<SessionCheckStatusResponse>(
       {
-        url: this.serverUrl + `/upload/status/${this.sessionInfo!.sessionId}`,
+        url: this.serverUrl + `/upload/status/${this.sessionInfo.sessionId}`,
         options: {
           credentials: 'include'
         },
@@ -229,12 +161,12 @@ export class FileSender {
     }
   }
 
-  private handleError(message: string): never {
+  protected handleError(message: string): never {
     this.onError && this.onError(message);
     throw new Error();
   }
 
-  private updateStatus(status: SendingStatus) {
+  protected updateStatus(status: SendingStatus) {
     if (this.status == status) return;
     this.status = status;
     this.onStatusUpdate && this.onStatusUpdate(status);
---
+++ src/API/FileSender/NewFileSender.ts
@@ -0,0 +1,58 @@
+import type { DefaultErrorBody } from "../apiRequest";
+import { SequentialChunkSelector } from "./ChunkSelectStrategy";
+import { FileSender, type SessionIniResponse } from "./FileSender";
+
+interface NewFileSenderConstruct {
+    file: File,
+    destinationDirId: number | 'root';
+    retriesCount: number;
+}
+
+export class NewFileSender extends FileSender {
+    public onSessionIni?: (sessionId: number, filePath: string) => void;
+
+    constructor(params: NewFileSenderConstruct) {
+        super(params.file, params.destinationDirId, params.retriesCount);
+    }
+
+    async start() {
+        const sessionInfo = await this.initialize();
+        this.sessionInfo = sessionInfo;
+
+        this.chunkSender = this.createChunkSender(sessionInfo, new SequentialChunkSelector(this.sessionInfo.chunksCount));
+
+        this.updateStatus('sending');
+        this.chunkSender.start();
+    }
+
+    private async initialize() {
+        this.updateStatus('preparing');
+        const sessionInfo = await this.sendInitializeRequest();
+        this.onSessionIni?.(sessionInfo.sessionId, sessionInfo.path);
+
+        return sessionInfo;
+    }
+
+    private async sendInitializeRequest(): Promise<SessionIniResponse> {
+        const errorHandler = (errorData: DefaultErrorBody) => {
+            this.updateStatus('cancel');
+            this.handleError(errorData.message);
+        };
+
+        const data = await this.apiClient<SessionIniResponse>({
+            url: `${this.serverUrl}/upload/init`,
+            options: {
+                credentials: "include",
+                method: "POST",
+                body: JSON.stringify({
+                    fileName: this.file.name,
+                    fileSize: this.file.size,
+                    destinationDirId: this.destinationDirId,
+                })
+            },
+            errorHandler,
+        });
+
+        return data;
+    }
+}
